685894808393b36763a4234e725881f7b18fb315
robinkotsch <75489778+robinkotsch@users.noreply.github.com>
2023-10-31 09:25:46 +0100

    added redisString

--- DotNetCoreSqlDb/Program.cs
+++ DotNetCoreSqlDb/Program.cs
@@ -4,13 +4,19 @@ using DotNetCoreSqlDb.Data;
 var builder = WebApplication.CreateBuilder(args);
 
 
-builder.Services.AddSqlServer<ApplicationDbContext>(builder.Configuration.GetConnectionString("AZURE_POSTGRESQL_CONNECTIONSTRING"));
+//builder.Services.AddSqlServer<ApplicationDbContext>(builder.Configuration.GetConnectionString("AZURE_POSTGRESQL_CONNECTIONSTRING"));
+
+
+// Add database context and cache
+builder.Services.AddDbContext<MyDatabaseContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("AZURE_POSTGRESQL_CONNECTIONSTRING")));
 
 
 
 builder.Services.AddStackExchangeRedisCache(options =>
 {
-options.Configuration = builder.Configuration["AZURE_REDIS_CONNECTIONSTRING"];
+//options.Configuration = builder.Configuration["AZURE_REDIS_CONNECTIONSTRING"];
+options.Configuration = "todoiiot - cache.redis.cache.windows.net:6380,password = xjlbKfO1ng7L4vMtdWq1uh3Bj5fTvPsVqAzCaEDlnrU =,ssl = True,defaultDatabase = 0"
 options.InstanceName = "SampleInstance";
 });
 
